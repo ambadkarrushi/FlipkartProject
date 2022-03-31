38faaf61438cbe9ea5e85529118d8d5e228e8938
Rushikesh Ambadkar <rushiambadkar97@gmail.com>
2022-03-31 14:07:05 +0530

    spaces

--- src/main/java/BaseClasses/BaseClass1.java
+++ src/main/java/BaseClasses/BaseClass1.java
@@ -16,10 +16,11 @@ public class BaseClass1 {
 		if(a.equals("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
-			//
-			//
-			//
-			//System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
+			
+			
+			
+			
+			System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\Browsers\\chromedriver.exe");
 			WebDriver driver = new ChromeDriver();     //upcasting
 			driver.get("https://www.flipkart.com/");
 			driver.manage().window().maximize();
